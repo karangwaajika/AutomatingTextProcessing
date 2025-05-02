5ce9d40defc6a98fa91a6e6eb74f22a5293ea78c
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-05-02 17:24:51 +0200

    set up the project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.automatictextprocessing {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.automatictextprocessing to javafx.fxml;
+    exports org.example.automatictextprocessing;
+}
